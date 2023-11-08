b57cf2d16a199d03c19a7bff4adf161626a3d2c5
Raphael Pierzina <raphael@hackebrot.de>
2023-11-08 13:08:13 +0100

    feat: add factory for dependency injection

--- metrics/go.mod
+++ metrics/go.mod
@@ -6,11 +6,15 @@ require (
 	github.com/google/go-cmp v0.6.0
 	github.com/shurcooL/githubv4 v0.0.0-20230704064427-599ae7bbf278
 	github.com/spf13/cobra v1.7.0
+	golang.org/x/oauth2 v0.13.0
 )
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/net v0.16.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
---
+++ metrics/internal/factory/factory.go
@@ -0,0 +1,65 @@
+package factory
+
+import (
+	"context"
+	"net/http"
+	"os"
+
+	"github.com/mozilla-services/rapid-release-model/metrics/internal/config"
+	"github.com/mozilla-services/rapid-release-model/metrics/internal/export"
+	"github.com/mozilla-services/rapid-release-model/metrics/internal/github"
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+)
+
+// Factory is used to create dependencies for the CLI application
+type Factory struct {
+	NewEncoder             func() (export.Encoder, error)
+	NewExporter            func() (export.Exporter, error)
+	NewGitHubHTTPClient    func() (*http.Client, error)
+	NewGitHubGraphQLClient func() (github.GraphQLClient, error)
+	NewGitHubRepo          func() (*github.Repo, error)
+}
+
+// NewFactory creates the default Factory for the CLI application
+func NewFactory(ctx context.Context) *Factory {
+	f := new(Factory)
+
+	f.NewEncoder = func() (export.Encoder, error) {
+		return &export.JSONEcoder{}, nil
+	}
+
+	f.NewExporter = func() (export.Exporter, error) {
+		encoder, err := f.NewEncoder()
+		if err != nil {
+			return nil, err
+		}
+		return &export.WriterExporter{W: os.Stdout, Encoder: encoder}, nil
+	}
+
+	f.NewGitHubHTTPClient = func() (*http.Client, error) {
+		src := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: config.FromEnv("GITHUB", "TOKEN")},
+		)
+		return oauth2.NewClient(ctx, src), nil
+	}
+
+	f.NewGitHubGraphQLClient = func() (github.GraphQLClient, error) {
+		httpClient, err := f.NewGitHubHTTPClient()
+		if err != nil {
+			return nil, err
+		}
+		gqlClient := githubv4.NewClient(httpClient)
+		return gqlClient, nil
+	}
+
+	f.NewGitHubRepo = func() (*github.Repo, error) {
+		repo := &github.Repo{
+			Owner: config.FromEnv("GITHUB", "REPO_OWNER"),
+			Name:  config.FromEnv("GITHUB", "REPO_NAME"),
+		}
+		return repo, nil
+	}
+
+	return f
+}
