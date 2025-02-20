699b573cbe901f212630eccdd65131b0a2ad7288
Raphael Aurich <raphael@hackebrot.de>
2025-02-20 14:47:13 +0100

    refactor: Define GitHub API interfaces and unified models

--- pkg/github/api.go
+++ pkg/github/api.go
@@ -4,13 +4,32 @@ import (
 	"context"
 )
 
-// Represents a GitHub repo
-type Repo struct {
-	Owner string
-	Name  string
+// PullRequestsService provides access to GitHub Pull Request functionality.
+type PullRequestsService interface {
+	QueryPullRequests(ctx context.Context, repo *Repo, limit int) ([]PullRequest, error)
 }
 
-// GraphQLClient is satisfied by the the githubv4.Client.
-type GraphQLClient interface {
-	Query(ctx context.Context, q interface{}, variables map[string]interface{}) error
+// DeploymentsService provides access to GitHub Deployment functionality.
+type DeploymentsService interface {
+	QueryDeployments(ctx context.Context, repo *Repo, envs *[]string, limit int) ([]Deployment, error)
+}
+
+// ReleasesService provides access to GitHub Release functionality.
+type ReleasesService interface {
+	QueryReleases(ctx context.Context, repo *Repo, limit int) ([]Release, error)
+}
+
+// CommitsComparisonService provides commit comparison functionality.
+type CommitsComparisonService interface {
+	CompareCommits(ctx context.Context, repo *Repo, base string, head string, limit int) (*CommitsComparison, error)
+}
+
+// RefComparisonService provides Git reference comparison functionality.
+type RefComparisonService interface {
+	QueryCompareRefs(ctx context.Context, repo *Repo, base string, head string, limit int) (*CommitsComparison, error)
+}
+
+// HistoryService provides commit history functionality.
+type HistoryService interface {
+	QueryHistory(ctx context.Context, repo *Repo, head string, base string, limit int) ([]Commit, error)
 }
